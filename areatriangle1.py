a30dd69574f73bacb2eb7ac291fb87b073776932
shreya-5611 <149505620+shreya-5611@users.noreply.github.com>
2023-11-01 23:40:40 +0530

    Add files via upload

---
+++ areatriangle1.py
@@ -0,0 +1,11 @@
+a= 5
+b = 6
+c = 7
+
+
+s = (a + b + c) / 2
+
+
+area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
+
+print("The area of the given triangle is %0.2f" % area)
---
+++ variable1.py
@@ -0,0 +1,7 @@
+number1 = 5
+
+number2 = 10
+ 
+
+large = number1 if number1 > number2 else number2
+print("the larger among these two is:",large)
