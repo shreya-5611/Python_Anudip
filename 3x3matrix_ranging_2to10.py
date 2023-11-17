796a1cc4e813a32c662e5becf362869512907231
shreya-5611 <149505620+shreya-5611@users.noreply.github.com>
2023-11-18 00:16:44 +0530

    Add files via upload

---
+++ 3x3matrix_ranging_2to10.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+matrix = np.arange(2, 11).reshape(3, 3)
+
+print("The 3x3 matrix array is:\n ",matrix)
+
+
+'''
+output:
+    The 3x3 matrix array is:
+  [[ 2  3  4]
+   [ 5  6  7]
+   [ 8  9 10]]
+
+'''
